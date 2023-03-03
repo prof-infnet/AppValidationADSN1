b4d41b27979516c82e7a4d4089a41e3a974016f7
JonhEdsonRibeirodeCa <jonh.carvalho@prof.infnet.edu.br>
2023-03-03 17:28:26 -0300

    Add project files.

---
+++ AppValidation/Models/JobApplication.cs
@@ -0,0 +1,11 @@
+﻿namespace AppValidation.Models
+{
+    public class JobApplication
+    {
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string Sex { get; set; }
+        public string Experience { get; set; }
+        public bool TermsAccepted { get; set; }
+    }
+}
